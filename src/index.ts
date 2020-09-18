36c66472833409f4dbb83d0876f583c61989553d
Kahngjoon Koh <46638829+kahngjoonkoh@users.noreply.github.com>
2020-09-18 13:48:30 +0400

    #2 configuations

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+const name = "John",
+  age = 16,
+  gender = "male";
+
+const sayHi = (name: string, age: number, gender: string): void => {
+  console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayHi(name, age, gender);
+
+export {};
