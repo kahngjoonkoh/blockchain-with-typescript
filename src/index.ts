53df88c7e835ac74d4b12387dc16f4e1bcaf92eb
Kahngjoon Koh <46638829+kahngjoonkoh@users.noreply.github.com>
2020-09-25 18:08:08 +0400

    before debugging

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
-const name = "John",
-  age = 16,
-  gender = "male";
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender?: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+}
 
-const sayHi = (name: string, age: number, gender: string): void => {
-  console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+const john = new Human("John", 16, "male");
+
+const sayHi = (person: Human): void => {
+  console.log(
+    `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`
+  );
 };
 
-sayHi(name, age, gender);
+sayHi(john);
 
 export {};
